7ccf5b0b672b44dafff7b0e907d37c9c26d4cd9e
Fabio Vige <fabiovige@gmail.com>
2024-09-02 12:43:55 -0300

    feat: refatoração do service e controller user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,23 +6,19 @@ use App\Http\Requests\UserRequest;
 use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\DB;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserController extends Controller
 {
     protected $userService;
-        
+    
     public function __construct(UserService $userService)
     {
         $this->userService = $userService;
     }
-    
-    public function index(Request $request)
+
+    public function index(Request $request): JsonResponse
     {
         try {
             $filters = $request->only([
@@ -30,14 +26,13 @@ class UserController extends Controller
                 'address', 'number', 'complement', 'neighborhood', 
                 'city', 'state'
             ]);
-            Log::info('Busca todos os usuários:', $request->all());
 
             $perPage = $request->input('per_page', 10);
             $users = $this->userService->getUsers($filters, $perPage);
 
             return response()->json($users, 200);
 
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             Log::error('Falha ao buscar usuários:', ['error' => $e->getMessage()]);
             return response()->json([
                 'status' => false,
@@ -46,70 +41,34 @@ class UserController extends Controller
         }
     }
 
-    public function store(UserRequest $request)
+    public function store(UserRequest $request): JsonResponse
     {
-        DB::beginTransaction();
-
         try {
-            
-            $user = User::create($request->all());
-            Log::info('Cadastrando um novo usuário:', $request->all());
+            $user = $this->userService->createUser($request->all());
 
-            DB::commit();
-            Log::info('Usuário cadastrado com sucesso!', ['user' => $user->id]);
-            
             return response()->json([
                 'status' => true,
                 'user' => $user,
                 'message' => 'Usuário cadastrado com sucesso!',
             ], 201);
 
-        } catch (Exception $e) {
-            
-            DB::rollBack();
-            Log::error('Falha ao cadastrar usuário:', ['error' => $e->getMessage()]);
-            
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => false,
                 'message' => 'Falha ao cadastrar usuário!'
             ], 500);
-
         }
     }
 
-    public function show(User $user)
+    public function show(User $user): JsonResponse
     {
-        try {
-            
-            Log::info('Buscando usuário com ID:', ['user_id' => $user->id]);
-            return response()->json($user);
-
-        } catch (Exception $e) {
-            Log::error('Falha ao exibir usuário com ID:', ['user_id' => $user->id, 'error' => $e->getMessage()]);
-            return response()->json([
-                'staus' => false,
-                'message' => 'Falha ao exibir usuário!'
-            ], 500);
-        }
+        return response()->json($user);
     }
 
-    /**
-     * 
-     * @param \App\Http\Requests\UserRequest $request
-     * @param \App\Models\User $user
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function update(UserRequest $request, User $user) : JsonResponse
+    public function update(UserRequest $request, User $user): JsonResponse
     {
-        DB::beginTransaction();
-
         try {
-            
-            $user->update($request->all());
-            Log::info('Usuário atualizado com ID:', ['user_id' => $user->id, 'request_data' => $request->all()]);
-
-            DB::commit();
-            Log::info('Usuário atualizado com sucesso!', ['user_id' => $user->id]);
+            $user = $this->userService->updateUser($user, $request->all());
 
             return response()->json([
                 'status' => true,
@@ -117,38 +76,25 @@ class UserController extends Controller
                 'message' => 'Usuário atualizado com sucesso!',
             ], 200);
 
-        } catch (Exception $e) {
-            
-            DB::rollBack();
-            Log::error('Falha ao atualizar o usuário:', ['user' => $user, 'error' => $e->getMessage()]);
-            
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => false,
                 'message' => 'Falha ao atualizar o usuário!'
             ], 500);
-
         }
     }
 
-    public function destroy(User $user)
+    public function destroy(User $user): JsonResponse
     {
-        DB::beginTransaction();
-
         try {
-            Log::info('Excluíndo usuário com ID:', ['user_id' => $user->id]);
-            $user->delete();
-            
-            DB::commit();
+            $this->userService->deleteUser($user);
 
             return response()->json([
                 'status' => true,
                 'message' => 'Usuário excluído com sucesso!'
             ], 204);
 
-        } catch (Exception $e) {
-            DB::rollBack();
-            Log::error('Falha ao excluir o usuário com ID:', ['user_id' => $user->id, 'error' => $e->getMessage()]);
-            
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => false,
                 'message' => 'Falha ao excluir o usuário!'
@@ -156,3 +102,4 @@ class UserController extends Controller
         }
     }
 }
+
