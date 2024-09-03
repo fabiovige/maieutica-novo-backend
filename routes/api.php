5cdeca7c835cf1c29e9932ee668f28e6b8a57f5c
Fabio Vige <fabiovige@gmail.com>
2024-09-03 08:13:22 -0300

    implementado cors, login e logout

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ChecklistController;
 use App\Http\Controllers\UserController;
 
 Route::post('login', [AuthController::class, 'login']);
+Route::post('logout/{user}', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // Rotas de CRUD para Users
-Route::apiResource('users', UserController::class)->middleware('auth:sanctum');;
+Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
